88cafbd3c00caa3c0385231ec6e8bedf72ef4a08
Jan Kalina <jan.kalina@fantom.foundation>
2021-09-28 11:46:40 +0200

    Extend testing data - add listing, offer

--- internal/graphql/resolvers/root.go
+++ internal/graphql/resolvers/root.go
@@ -6,12 +6,10 @@ import (
 	"artion-api-graphql/internal/config"
 	"artion-api-graphql/internal/logger"
 	"artion-api-graphql/internal/repository"
-	"artion-api-graphql/internal/types"
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"golang.org/x/sync/singleflight"
-	"math/big"
 	"sync"
 )
 
@@ -114,33 +112,4 @@ func (rs *RootResolver) Tokens(args struct{ PaginationInput }) (con *TokenConnec
 		return nil, err
 	}
 	return NewTokenConnection(list)
-}
-
-// FOR TESTING ONLY! remove before production use!
-func (rs *RootResolver) PushTestingData() (*string, error) {
-
-	tok := types.Token{
-		Nft: common.HexToAddress("0xf41270836dF4Db1D28F7fd0935270e3A603e78cC"),
-		TokenId: hexutil.Big(*big.NewInt(9292)),
-		Uri: "ipfs://QmTetVgMNVGj88s9NQuANyVmjMtZqhZDp8T21huiVGbfAi",
-	}
-	tok.GenerateId()
-	err := repository.R().StoreToken(&tok)
-	if err != nil {
-		log.Errorf("error in storing token", err)
-	}
-
-	tok = types.Token{
-		Nft: common.HexToAddress("0x61af4d29f672e27a097291f72fc571304bc93521"),
-		TokenId: hexutil.Big(*big.NewInt(3224)),
-		Uri: "https://artion1.mypinata.cloud/ipfs/QmcAtMen7niz53eHpnehWJvYPVEMkxaT1y8mtkPjz5rAwf",
-	}
-	tok.GenerateId()
-	err = repository.R().StoreToken(&tok)
-	if err != nil {
-		log.Errorf("error in storing token", err)
-	}
-
-	out := "Loaded OK"
-	return &out, nil
 }
