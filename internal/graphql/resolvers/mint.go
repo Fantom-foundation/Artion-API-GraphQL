b2adbc62a50195ed01318b41ae2d77ea076d4fa2
Jan Kalina <jan.kalina@fantom.foundation>
2021-11-08 20:49:24 +0100

    EstimateMintFeeGas - errors as data

--- internal/graphql/resolvers/mint.go
+++ internal/graphql/resolvers/mint.go
@@ -5,9 +5,11 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"math/big"
+	"strings"
 )
 
 type MintFeeGas struct {
+	Error *string
 	PlatformFee *hexutil.Big
 	Gas *hexutil.Big
 }
@@ -19,9 +21,17 @@ func (rs *RootResolver) EstimateMintFeeGas(args struct{
 }) (*MintFeeGas, error) {
 	platformFee, gas, err := repository.R().EstimateMintGas(args.User, args.Contract, args.TokenUri)
 	if err != nil {
-		return nil, err
+		log.Warningf("failed to estimate mint fee/gas for %s: %s", args.Contract, err)
+		errStr := err.Error()
+		errStr = strings.TrimPrefix(errStr, "execution reverted: ")
+		return &MintFeeGas{
+			Error:       &errStr,
+			PlatformFee: nil,
+			Gas:         nil,
+		}, nil
 	}
 	return &MintFeeGas{
+		Error:       nil,
 		PlatformFee: (*hexutil.Big)(platformFee),
 		Gas:         (*hexutil.Big)(new(big.Int).SetUint64(gas)),
 	}, nil
