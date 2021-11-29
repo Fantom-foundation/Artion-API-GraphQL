f475d22e2dde3515375c6afc707fe0879c85fa52
Jan Kalina <jan.kalina@fantom.foundation>
2021-11-29 10:45:37 +0100

    Rescan script - check arguments

--- doc/rescan.sh
+++ doc/rescan.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+if [ $# -eq 0 ]; then
+    echo "This script start re-scanning of Artion database"
+    echo "Use as: sudo $0 (block-number) (reason-for-log)"
+    exit 1
+fi
+if [ $USER != "root" ]; then
+    echo "Script must be run as root"
+    exit 1
+fi
+
 echo "Starting re-scanning from block $1, reason: $2"
 version=`/home/opera/go/src/Artion-API-GraphQL/build/artionapi -v 2>/dev/null | grep "Commit Hash:"`
 time=`date --iso-8601=seconds`
@@ -10,4 +20,5 @@ echo "$log" >> /var/log/artion-scan.log
 mongo --eval "db.status.update({ _id: \"lastSeenBlock\" }, { \$set: { block: NumberLong($1) } });" artion
 systemctl start artion.service
 systemctl status artion.service
+exit 0
 
