2389d6b84e5bd3036ae717f11f7340dc7db7a42c
Jan Kalina <jan.kalina@fantom.foundation>
2021-12-20 10:25:19 +0100

    Downloader - dont use default http client instance

--- internal/repository/uri/downloader.go
+++ internal/repository/uri/downloader.go
@@ -171,8 +171,9 @@ func (d *Downloader) getFromIpfsGateway(uri string) (data []byte, mimetype strin
 
 // getFromHttp downloads the file from HTTP.
 func (d *Downloader) getFromHttp(uri string) (data []byte, mimetype string, err error) {
-	client := http.DefaultClient
-	client.Timeout = 20 * time.Second
+	client := http.Client{
+		Timeout: 20 * time.Second,
+	}
 
 	resp, err := client.Get(uri)
 	if err != nil {
