1c98b2ac9b7d406431856c28219b05a88b332c87
Jan Kalina <jan.kalina@fantom.foundation>
2022-01-30 19:31:45 +0100

    Add Withdraw2MonthsAfterStart into auction props

--- internal/types/auction_props.go
+++ internal/types/auction_props.go
@@ -4,6 +4,7 @@ type AuctionProps struct {
 	WinnerCanResult            bool // can the winner result the auction instead of the seller?
 	SellerCanResultUnderpriced bool // can seller result auction when topBid is less than reserve price?
 	CanCancelSuccessful        bool // can seller cancel auction when topBid is more than reserve price?
+	Withdraw2MonthsAfterStart  bool // can bidder withdraw the bid 2 months after the auction START?
 	HasResultFailed            bool // has the contract "resultFailedAuction" method?
 }
 
@@ -11,6 +12,7 @@ var AuctionV1Props = AuctionProps{
 	WinnerCanResult:            false,
 	SellerCanResultUnderpriced: false,
 	CanCancelSuccessful:        true,
+	Withdraw2MonthsAfterStart:  true,
 	HasResultFailed:            false,
 }
 
@@ -18,6 +20,7 @@ var AuctionV2Props = AuctionProps{
 	WinnerCanResult:            true,
 	SellerCanResultUnderpriced: false,
 	CanCancelSuccessful:        false,
+	Withdraw2MonthsAfterStart:  false,
 	HasResultFailed:            true,
 }
 
@@ -25,5 +28,6 @@ var AuctionV3Props = AuctionProps{
 	WinnerCanResult:            true,
 	SellerCanResultUnderpriced: true,
 	CanCancelSuccessful:        false,
+	Withdraw2MonthsAfterStart:  false,
 	HasResultFailed:            true,
 }
