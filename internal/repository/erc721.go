68a1bde6961c10586289bef7e9f323530ebbfd80
Jiri Malek <jirka.malek@gmail.com>
2022-04-13 10:44:33 +0200

    Reduce ERC-721 contract requirements.

--- internal/repository/erc721.go
+++ internal/repository/erc721.go
@@ -11,12 +11,12 @@ const (
 	Erc721BaseInterfaceID = "0x80ac58cd"
 
 	// Erc721MetadataInterfaceID represents an identifier of the ERC-721 Metadata interface in ERC-165 encoding.
-	Erc721MetadataInterfaceID = "0x5b5e139f"
+	// Erc721MetadataInterfaceID = "0x5b5e139f"
 )
 
 // IsErc721Contract checks if the given address is a contract with ERC-721 interface support.
 func (p *Proxy) IsErc721Contract(adr *common.Address) bool {
-	return p.rpc.SupportsInterface(adr, Erc721BaseInterfaceID) && p.rpc.SupportsInterface(adr, Erc721MetadataInterfaceID)
+	return p.rpc.SupportsInterface(adr, Erc721BaseInterfaceID) //  && p.rpc.SupportsInterface(adr, Erc721MetadataInterfaceID)
 }
 
 // Erc721StartingBlockNumber provides the first important block number for the ERC-721 contract.
