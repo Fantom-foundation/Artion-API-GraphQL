cceb88d177cb2b36aef72691aeed85f910ce3cdd
Jan Kalina <jan.kalina@fantom.foundation>
2022-08-01 21:24:45 +0200

    Modify Mongo export order

--- doc/export.sh
+++ doc/export.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# export info about last processed block first
+mongoexport --db=artion --collection=status --out status.json
+
+# export scanned data next
 mongoexport --db=artion --collection=activities --out activities.json
 mongoexport --db=artion --collection=auctions --out auctions.json
 mongoexport --db=artion --collection=bids --out bids.json
@@ -7,9 +11,9 @@ mongoexport --db=artion --collection=listings --out listings.json
 mongoexport --db=artion --collection=observed --out observed.json
 mongoexport --db=artion --collection=offers --out offers.json
 mongoexport --db=artion --collection=ownerships --out ownerships.json
-mongoexport --db=artion --collection=status --out status.json
 mongoexport --db=artion --collection=tokens --out tokens.json
 mongoexport --db=artion --collection=burns --out burns.json
+
+# export notification data as the last
 mongoexport --db=artion --collection=notification_tpl --out notification_tpl.json
 mongoexport --db=artion --collection=notifications --out notifications.json
-
